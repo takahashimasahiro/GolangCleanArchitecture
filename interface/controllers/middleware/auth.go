f7ce3ca16f982b01ad3f10d5b0385e9d16fc7de4
MTakahashi <takahashi@dptr.jp>
2019-11-08 18:54:42 +0900

    update

--- interface/controllers/middleware/auth.go
+++ interface/controllers/middleware/auth.go
@@ -1,2 +1,13 @@
 package middleware
 
+import (
+	"context"
+
+	"../database"
+	"../dcontext"
+	"../network"
+)
+
+type middleware struct {
+	
+}
--- interface/database/user_repository.go
+++ interface/database/user_repository.go
@@ -32,20 +32,20 @@ func (userRepository *userRepository) Store(user domain.User) error {
 
 // AuthTokenを条件にレコード取得
 func (userRepository *userRepository) FindByAuthToken(authToken string) (*domain.User, error) {
-	row := userRepository.db.QueryRow("SELECT * FROM user WHERE auth_token=?", suthToken)
+	row := userRepository.db.QueryRow("SELECT * FROM user WHERE auth_token=?", authToken)
 	return ConvertToUser(row)
 }
 
 // UserIDを条件にレコードを取得
 func (userRepository *userRepository) FindByUserID(userID string) (*domain.User, error) {
-	row := userRepository.db.QueryRow("SELECT * FROM user WHERE auth_token", authToken)
+	row := userRepository.db.QueryRow("SELECT * FROM user WHERE user_id=?", userID)
 	return ConvertToUser(row)
 }
 
 // UserIDを条件にレコードを更新する
-func (userRepository *userRepository) UpdateByUserID(userID string) (*domain.User, error){
-	_, row := userRepository.db.Exec("UPDATE user SET name=? WHERE user_id=?", name, userID)
-	if err != nil{
+func (userRepository *userRepository) UpdateByUserID(userID string, name string) error {
+	_, err := userRepository.db.Exec("UPDATE user SET name=? WHERE user_id=?", name, userID)
+	if err != nil {
 		log.Println(err)
 		return err
 	}
